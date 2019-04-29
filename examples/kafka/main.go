670494da626514a6d9e1941ab4d01c17c93f309e
殷光飞 <yinguangfei@admaster.com.com>
2019-04-29 16:34:58 +0800

    更新producer

--- examples/kafka/main.go
+++ examples/kafka/main.go
@@ -30,24 +30,63 @@ func main() {
 		}
 	}()
 
-	// producer
-	producerConfig := sarama.NewConfig()
-	producerConfig.Producer.Retry.Max = 5
-	producerConfig.Producer.Retry.Backoff = time.Minute * 3
-	producer, err := kafka.NewProducer([]string{"127.0.0.1:9092"}, producerConfig)
+	// sync producer
+	syncConfig := sarama.NewConfig()
+	syncConfig.Producer.Retry.Max = 5
+	syncConfig.Producer.RequiredAcks = sarama.WaitForAll
+	syncConfig.Producer.Retry.Backoff = time.Minute * 3
+	syncConfig.Producer.Return.Successes = true
+	syncProducer, err := kafka.NewSyncProducer([]string{"127.0.0.1:9092"}, syncConfig)
 	if err != nil {
-		fmt.Println("New kafka producer error:", err)
+		fmt.Println("New kafka sync producer error:", err)
 		return
 	}
-	producer.Setup("topic_test")
-	defer producer.Destroy()
+
+	// async producer
+	asyncConfig := sarama.NewConfig()
+	asyncConfig.Producer.Retry.Max = 5
+	//等待服务器所有副本都保存成功后的响应
+	asyncConfig.Producer.RequiredAcks = sarama.WaitForAll
+	asyncConfig.Producer.Retry.Backoff = time.Minute * 3
+	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
+	asyncConfig.Producer.Return.Successes = true
+	asyncConfig.Producer.Return.Errors = true
+	asyncProducer, err := kafka.NewAsyncProducer([]string{"127.0.0.1:9092"}, asyncConfig)
+	if err != nil {
+		fmt.Println("New kafka async producer error:", err)
+		return
+	}
+	// return.Successes,return.Errors 为true 需要处理error 和 succedss,否则会阻塞
+	go func() {
+		for {
+			select {
+			case suc := <-asyncProducer.Successes():
+				fmt.Println("async publish offset: ", suc.Offset, "timestamp: ", suc.Timestamp.String(), "partitions: ", suc.Partition)
+			case fail := <-asyncProducer.Errors():
+				if fail != nil {
+					fmt.Println("async send error:", fail.Error())
+				}
+			}
+		}
+	}()
+
+	defer syncProducer.Destroy()
+	defer asyncProducer.Destroy()
 	defer consumer.Destroy()
 	ticker := time.NewTicker(time.Second * 5)
 	sendCount := 0
 	for _ = range ticker.C {
 		msgStr := fmt.Sprintf("%d", sendCount)
-		// send message
-		producer.Send() <- []byte(msgStr)
+
+		// sync send message
+		_, _, err := syncProducer.Send("topic_test", []byte(msgStr))
+		if err != nil {
+			fmt.Println("send message failed,", err)
+			continue
+		}
+
+		// async send message
+		asyncProducer.Send("topic_test", []byte(msgStr))
 		sendCount++
 		fmt.Printf("send message count: %v,%v\n", sendCount, time.Now())
 	}
--- examples/nsq/main.go
+++ examples/nsq/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	consumer, err := nsq.NewConsumer("192.168.4.130:4161", "192.168.4.130:4150", "test_topic", "channel")
+	consumer, err := nsq.NewConsumer("127.0.0.1:4161", "127.0.0.1:4150", "test_topic", "channel")
 	if err != nil {
 		fmt.Println("New nsq consumer error:", err)
 		return
@@ -22,22 +22,22 @@ func main() {
 	}()
 
 	// producer
-	producer, err := nsq.NewProducer("192.168.4.130:4150")
+	producer, err := nsq.NewProducer("127.0.0.1:4150")
 	if err != nil {
 		fmt.Println("New nsq  producer error:", err)
 		return
 	}
-	producer.Setup("test_topic", time.Second*5, true)
+
 	defer producer.Destroy()
 	defer consumer.Destroy()
 
-	// send message,仅支持PublishAsync,后期改善
+	// send message
 	sendCount := 0
 	ticker := time.NewTicker(time.Second * 1)
 	for _ = range ticker.C {
 		msgStr := fmt.Sprintf("%d", sendCount)
 		// send message
-		producer.Send() <- []byte(msgStr)
+		producer.Send("test_topic", []byte(msgStr), time.Second*3, true)
 		sendCount++
 		fmt.Printf("send message count: %v,%v\n", sendCount, time.Now())
 	}
